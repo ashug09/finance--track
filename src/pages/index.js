b73654683fa8de943e3f646a3ca5a23d3f7ef86b
ashug09 <79792289+ashug09@users.noreply.github.com>
2024-02-26 11:28:40 +0530

    made "add expense" day 3
    added shadcn UI. added it's input components. added categories display on the mobile home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,32 +4,36 @@ import Desktop_nav from "./desktop_nav";
 import Recent_transaction from "./recent_transaction";
 import Desktop_piechart from "./desktop_piechart";
 import Group from "./group";
-import {isMobile} from 'react-device-detect';
+import { isMobile } from "react-device-detect";
 import Mobile_nav from "./mobile_nav";
 import Mobile_total_spending from "./mobile_total_spending";
 import Mobile_piechart from "./mobile_piechart";
 import Mobile_group from "./mobile_group";
+import Mobile_category from "./mobile_category";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  if (isMobile){
-    return(
+  if (isMobile) {
+    return (
       <>
-      <Mobile_nav/>
-      <Mobile_total_spending/>
-      <Mobile_piechart/>
-      <Mobile_group/>
+        <div className="mb-20">
+          <Mobile_nav />
+          <Mobile_total_spending />
+          <Mobile_piechart />
+          <Mobile_group />
+          <Mobile_category />
+        </div>
       </>
-    )
+    );
   }
   return (
     <div className="mx-8 mt-2">
-      <Desktop_nav/>
+      <Desktop_nav />
       <div className="flex justify-around">
-        <Recent_transaction/>
-        <Desktop_piechart/>
+        <Recent_transaction />
+        <Desktop_piechart />
       </div>
-      <Group/>
+      <Group />
     </div>
   );
 }
--- src/pages/mobile_add_expense.jsx
+++ src/pages/mobile_add_expense.jsx
@@ -1,4 +1,14 @@
+import { Input } from "@/components/ui/input";
 import React from "react";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
 
 export default function Mobile_add_expense() {
   return (
@@ -9,8 +19,8 @@ export default function Mobile_add_expense() {
       <div className="flex justify-center my-8">
         <h1 className="text-center text-4xl">₹ </h1>
         <input
-          className="w-max text-center"
-          type="number"
+          className="w-max text-center outline-none"
+          type="tel"
           placeholder="00.00"
         />
       </div>
@@ -19,34 +29,60 @@ export default function Mobile_add_expense() {
       <div>
         <div className="flex justify-center bg-red-100 p-4 mx-20 my-4 rounded-2xl">
           {/* group input drop down */}
-          <div>Buddy Group</div>
+          <Select className="outline-none">
+            <SelectTrigger className="w-[180px]">
+              <SelectValue className="outline-none" placeholder="Select Group" />
+            </SelectTrigger>
+            <SelectContent className="outline-none">
+              <SelectItem className="outline-none" value="Personnel">Personnel</SelectItem>
+              <SelectItem className="outline-none" value="Buddy">Buddy</SelectItem>
+              <SelectItem className="outline-none" value="College">College</SelectItem>
+              <SelectItem className="outline-none" value="Cousin">Cousin</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
         <div>
           <div className="flex justify-center bg-blue-100 p-4 mx-20 my-4 rounded-2xl">
             {/* category input drop down */}
-            <div>Jain Shikanji</div>
+            <Select className="outline-none">
+            <SelectTrigger className="w-[180px]">
+              <SelectValue className="outline-none" placeholder="Category" />
+            </SelectTrigger>
+            <SelectContent className="outline-none">
+              <SelectItem className="outline-none" value="Food">Food</SelectItem>
+              <SelectItem className="outline-none" value="Entertainment">Entertainment</SelectItem>
+              <SelectItem className="outline-none" value="Wellness">Wellness</SelectItem>
+              <SelectItem className="outline-none" value="Lifestyle">Lifestyle</SelectItem>
+            </SelectContent>
+          </Select>
           </div>
         </div>
       </div>
+      <div>
+      <Button className="flex mx-auto my-4" size="lg">Add Expense</Button>
+
+      </div>
 
       {/* number input keypad input */}
-      <div className="">
-      <div className="grid grid-cols-3 mx-auto justify-center">
-        <h1 className="text-center text-4xl mt-5">1</h1>
-        <h2 className="text-center text-4xl mt-5">2</h2>
-        <h2 className="text-center text-4xl mt-5">3</h2>
-        <h2 className="text-center text-4xl mt-8">4</h2>
-        <h2 className="text-center text-4xl mt-8">5</h2>
-        <h2 className="text-center text-4xl mt-8">6</h2>
-        <h2 className="text-center text-4xl mt-8">7</h2>
-        <h2 className="text-center text-4xl mt-8">8</h2>
-        <h2 className="text-center text-4xl mt-8">9</h2>
-        <h2 className="text-center text-4xl mt-8">C</h2>
-        <h2 className="text-center text-4xl mt-8">0</h2>
-        <h2 className="text-center text-4xl mt-8">D</h2>
-      </div>
-<button className="bg-black text-white text-2xl w-72 mx-auto rounded-lg p-4 mt-5 flex justify-center">Add</button>
-      </div>
+      {/* <div className="">
+        <div className="grid grid-cols-3 mx-auto justify-center">
+          <h1 className="text-center text-4xl mt-5">1</h1>
+          <h2 className="text-center text-4xl mt-5">2</h2>
+          <h2 className="text-center text-4xl mt-5">3</h2>
+          <h2 className="text-center text-4xl mt-8">4</h2>
+          <h2 className="text-center text-4xl mt-8">5</h2>
+          <h2 className="text-center text-4xl mt-8">6</h2>
+          <h2 className="text-center text-4xl mt-8">7</h2>
+          <h2 className="text-center text-4xl mt-8">8</h2>
+          <h2 className="text-center text-4xl mt-8">9</h2>
+          <h2 className="text-center text-4xl mt-8">C</h2>
+          <h2 className="text-center text-4xl mt-8">0</h2>
+          <h2 className="text-center text-4xl mt-8">D</h2>
+        </div>
+        <button className="bg-black text-white text-2xl w-72 mx-auto rounded-lg p-4 mt-5 flex justify-center">
+          Add
+        </button>
+      </div> */}
     </div>
   );
 }
