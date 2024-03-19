bcc3086e03268c4346a0639a750226ea6ea839de
ashug09 <79792289+ashug09@users.noreply.github.com>
2024-03-19 23:03:16 +0530

    Day 15
    added protected routes, and added access token verification endpoint, so that if a user is having valid cookie then they don't have to log in again and again

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,20 +3,63 @@ import { Fredoka } from "next/font/google";
 import Mobile_nav from "./mobile/mobile_nav";
 import { useRouter } from "next/router";
 import { isMobile } from "react-device-detect";
+import SplashScreen from "./splashScreen";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import LoginForm from "./mobile/auth/login";
+import Register from "./mobile/auth/register";
+import axios from "axios";
 
 // If loading a variable font, you don't need to specify the font weight
 const fredoka = Fredoka({ subsets: ["latin"] });
 export default function App({ Component, pageProps }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const router = useRouter();
-  return (
+
+  useEffect(() => {
+    const accessTokenValue = Cookies.get("accessToken");
+    if (accessTokenValue) {
+      axios
+        .post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/user/verifytoken`, {
+          token: accessTokenValue,
+        })
+        .then((response) => {
+          // response.data === true
+          //   ? setIsAuthenticated(true)
+          //   : console.log(response.data);
+          setIsAuthenticated(response.data);
+          // router.reload()
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, []);
+
+  return isAuthenticated ?(
     <main className={fredoka.className}>
       {isMobile ? (
-        <div className={`${router.pathname === "/" || router.pathname === "/mobile/auth/login" || router.pathname === "/mobile/auth/register" || router.pathname === "/mobile/mobile_add_expense" ?  "hidden" : null}`}>
+        <div
+          className={`${
+            router.pathname === "/" ||
+            router.pathname === "/mobile/auth/login" ||
+            router.pathname === "/mobile/auth/register" ||
+            router.pathname === "/mobile/mobile_add_expense"
+              ? "hidden"
+              : null
+          }`}
+        >
           <Mobile_nav />
         </div>
       ) : null}
 
       <Component {...pageProps} />
+    </main>
+  ) : (
+    <main className={fredoka.className}>
+      {router.pathname === "/mobile/auth/login" ? <LoginForm /> : null}
+      {router.pathname === "/mobile/auth/register" ? <Register /> : null}
+      <SplashScreen />
     </main>
   );
 }
